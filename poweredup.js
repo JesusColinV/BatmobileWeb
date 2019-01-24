7cbc7784f0931cd19cd6513b59aadcdf1bf2f4a6
Samleo <ec3team@gmail.com>
2019-01-24 22:44:22 +0800

    Updated with connected

--- poweredup.js
+++ poweredup.js
@@ -1,26 +1,52 @@
 var PoweredUp = function(){
+	this.device = null;
+	this.connected = false;
+	this.server = null;
+	this.service = null;
+	this.serviceID = '00001623-1212-efde-1623-785feabcd123';
+	this.characteristic = null;
+
 	this.connect = function(){
+		var self = this;
 		return new Promise(async function(resolve, reject){
             try {
-    			this.device = await navigator.bluetooth.requestDevice({
-        			filters: [
-        				{ namePrefix: 'HUB' }
-        			],
+    			self.device = await navigator.bluetooth.requestDevice({
+        			filters: [],
         			optionalServices: [
-        				'00001623-1212-efde-1623-785feabcd123'
+        				self.serviceID.toString()
         			]
     			});
-    			_connected = true;
+
+				self.connected = true;
+				log("Connected: "+self.connected);
+
+				self.device.addEventListener('gattserverdisconnected', self.disconnect.bind(self));
+
+				log("Awaiting GATT connection...");
+
+				self.server = await self.device.gatt.connect();
+
+				log(self.server)
+				//self.service = await self.server.getPrimaryService(self.serviceID);
+				//sef.characteristic = await service.getCharacteristic('00001624-1212-efde-1623-785feabcd123');
 
                 resolve();
     		}
     		catch(error) {
     			log('Could not connect! ' + error);
-    			_connected = false;
+    			self.connected = false;
 
                 reject();
     		}
         });
+	};
+
+	this.disconnect = function(){
+		this.connected = false;
+	}
+
+	this.isConnected = function(){
+		return this.connected;
 	}
 };
 
