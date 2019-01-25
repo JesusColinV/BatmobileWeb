4d791ca516c388edc72f1be5494f7004d85ed402
Samleo <ec3team@gmail.com>
2019-01-25 08:35:22 +0800

    Updated bluetooth connection code

--- poweredup.js
+++ poweredup.js
@@ -3,20 +3,30 @@ var PoweredUp = function(){
 	this.connected = false;
 	this.server = null;
 	this.service = null;
-	this.serviceID = '00001623-1212-efde-1623-785feabcd123';
+	this.serviceID = 'e0cbf06c-cd8b-4647-bb8a-263b43f0f974';
 	this.characteristic = null;
+
+/*
+Bluetooth LE Device 90842b092e7f
+Base Container ID: {1624ac60-7fca-5415-bfa4-f4797714efa2}
+Class GUID: {e0cbf06c-cd8b-4647-bb8a-263b43f0f974}
+Association Endpoint Address: 90:84:2b:09:2e:7f
+
+*/
 
 	this.connect = function(){
 		var self = this;
 		return new Promise(async function(resolve, reject){
             try {
     			self.device = await navigator.bluetooth.requestDevice({
-        			filters: [
-						{ namePrefix: 'HUB' }
-						//, { services: self.serviceID.toString() }
-					],
-        			optionalServices: [
-        				'battery_service'
+        			/*"filters": [
+						//{ namePrefix: 'HUB' }
+						{ "services": self.serviceID }
+					],*/
+					"acceptAllDevices":true,
+        			"optionalServices": [
+        				'battery_service',
+						'e0cbf06c-cd8b-4647-bb8a-263b43f0f974'
         			]
     			});
 
